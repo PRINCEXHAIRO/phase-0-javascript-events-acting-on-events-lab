a63797f5b00921fd9ca7c39b6dccfde9c6147107
Hairo Barnard <Hairo.barnard@gmail.com>
2023-11-05 11:34:21 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+const dodger = document.querySelector('#dodger')
+
+console.log(dodger)
+
+dodger.style.background = "#FF69B4"
+
+dodger.style["bottom"] = "0px"
+
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+});
+
+function moveDodgerLeft(){
+const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  if(left > 0 ) { 
+    dodger.style.left = `${left - 1}px`;
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  })
+};
+
+function moveDodgerRight () { 
+   
+    const leftNumbers = dodger.style.left.replace("px","")
+    
+    const left = parseInt(leftNumbers, 10)
+  
+    if(left > 0) { 
+    dodger.style.left = `${left + 1}px`
+  }
+  document.addEventListener('Keydown', function(e) { 
+    if(e.key === "ArrowRight") { 
+       moveDodgerRight();
+    }
+  });
+};
